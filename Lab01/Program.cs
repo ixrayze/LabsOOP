dcd41aa274a363da0719404bba544b0184c3a9f0
Ihor <nebelicaigor93@gmail.com>
2026-09-15 13:19:16 +0300

    Lab01 Task05

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -7,8 +7,8 @@ internal class Program
         // Task1.Run();
         // Task2.Run();
         // Task3.Run();
-        Task4.Run();
-        // Task5.Run();
+        // Task4.Run();
+        Task5.Run();
         // Task6.Run();
         // Task7.Run();
         // Task8.Run();
---
+++ Lab01/Task5.cs
@@ -0,0 +1,24 @@
+namespace Lab01;
+
+public class Task5
+{
+    public static void Run()
+    {
+        Console.Write("Введіть день тижня (1-7): ");
+        int day = int.Parse(Console.ReadLine()!);
+
+        string info = day switch
+        {
+            1 => "Понеділок, 08:00–18:00",
+            2 => "Вівторок, 08:00–18:00",
+            3 => "Середа, 09:00–17:00",
+            4 => "Четвер, 08:00–18:00",
+            5 => "П'ятниця, 08:00–16:00",
+            6 => "Субота, 09:00–14:00",
+            7 => "Неділя — вихідний",
+            _ => "невідомий день"
+        };
+
+        Console.WriteLine($"День: {info}");
+    }
+}
