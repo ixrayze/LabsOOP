3b4ae2e078f78a1f458a247b1c88ee6034dd4ac1
Ihor <nebelicaigor93@gmail.com>
2026-09-15 12:42:16 +0300

    Lab01 Task03

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -5,8 +5,8 @@ internal class Program
     static void Main(string[] args)
     {
         // Task1.Run();
-        Task2.Run();
-        // Task3.Run();
+        // Task2.Run();
+        Task3.Run();
         // Task4.Run();
         // Task5.Run();
         // Task6.Run();
