f4dd084d087ed10c9a5fa641131044273f912fe4
Ihor <nebelicaigor93@gmail.com>
2026-09-15 12:15:36 +0300

    Lab01: project

---
+++ Lab01/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab01;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Task1.Run();
+        // Task2.Run();
+        // Task3.Run();
+        // Task4.Run();
+        // Task5.Run();
+        // Task6.Run();
+        // Task7.Run();
+        // Task8.Run();
+    }
+}
